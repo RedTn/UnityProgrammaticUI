59fe3bb1a6b7d6ee4ff9b7c0379052d03276bdda
RedTn <redmond.tran@gmail.com>
2016-11-02 23:20:16 -0700

    first commit

---
+++ Assets/HitEffectConstructor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HitEffectConstructor : MonoBehaviour {
+
+    public Sprite spriteToUse;
+    public bool hasRandomMovement;
+    public float decayTime;
+
+	// Use this for initialization
+	void Start () {
+        Decay decayScript = this.GetComponent<Decay>();
+        if(decayScript != null)
+        {
+            decayScript.decaySeconds = decayTime;
+        }
+
+        if(hasRandomMovement)
+        {
+            this.gameObject.AddComponent<MoveInRandomDirection>();
+        }
+        Image imageScript = this.GetComponent<Image>();
+        imageScript.sprite = spriteToUse; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/QuickPow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QuickPow : MonoBehaviour {
+
+    public List<GameObject> hitEffects = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	    if(hitEffects.Count < 1)
+        {
+            Debug.Log("You must add at least one hit effect");
+            this.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = Input.mousePosition;
+            GameObject powObject = Instantiate(hitEffects[Random.Range(0, hitEffects.Count)], clickPosition, Quaternion.identity) as GameObject;
+            powObject.transform.SetParent(this.transform);
+        }
+	}
+}
